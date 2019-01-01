5626bac18342cca2bd7ac369627c9255afa77a45
18868814391 <605729908@qq.com>
2019-01-01 14:12:54 +0800

    11

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,27 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Title extends Component{
+  render(){
+    return(
+      <b>i am title</b>
+    )
+  }
+}
+
+class App extends Component {  
+  fun1(aa,bb){
+    const flag=true;
+    return flag?aa:bb
+  }
+  fun2(e){
+    console.log(e.target.innerHTML,this)
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <div>
+          <p onClick={this.fun2.bind(this)}>
+            2333{this.fun1(<i>qq</i>,<b>ww</b>)}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Title/>
+          <Footer/>
+          <Like/>
       </div>
     );
+  }
+}
+
+class Footer extends Component{
+  render(){
+    return(
+      <div>
+        i am footer
+      </div>
+    )
+  }
+}
+
+class Like extends Component{
+  constructor(){
+    super();
+    this.state={is:true};
+    this.handerClick  = this.handerClick.bind(this);
+  }
+  handerClick(){
+    this.setState({
+      is: !this.state.is
+    })
+  }
+  render(){
+    return(
+      <button onClick={this.handerClick}>
+        {this.state.is?'喜欢':'讨厌'}
+      </button>
+    )
   }
 }
 
