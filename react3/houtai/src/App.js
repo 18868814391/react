08cd9d58aac0280f9a4d877ac586bfa4b4a52cfd
pptvvx <605729908@qq.com>
2019-01-27 21:18:25 +0800

    111

--- react3/houtai/src/App.js
+++ react3/houtai/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'antd';
+import 'antd/dist/antd.css';
 
 class App extends Component {
   render() {
@@ -17,7 +19,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React<Button>233</Button>
           </a>
         </header>
       </div>
